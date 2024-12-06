0a475e06b3325537d39e34441a0b559358a434bc
Karryun <165464282+Karryun@users.noreply.github.com>
2024-12-06 22:52:58 +0900

    feat: 복구???

--- ai-web-back/api/urls.py
+++ ai-web-back/api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("review", views.review, name="review"),
+    path("review/", views.review, name="review"),
     path("review/<int:reviewId>", views.review_id, name = "review_id"),
     path("check/<int:checkId>", views.check, name = "check")
 ]
